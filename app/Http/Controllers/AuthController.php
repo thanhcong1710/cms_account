97ea4190e1201126e97c484b0a64f2227a184ecc
conglt <thanhcong1710@gmail.com>
2023-09-23 22:42:24 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -77,7 +77,7 @@ class AuthController extends Controller
             }
         }
 
-        $connection->update(DB::raw("UPDATE users SET sip_id='".($sip_id ? $sip_id : null)." WHERE hrm_id=".$request->hrm_id));
+        $connection->update(DB::raw("UPDATE users SET sip_id= ".($sip_id ? "'".$sip_id."'" : null)." WHERE hrm_id= '".$request->hrm_id."'"));
 
         return $this->respondWithToken($token, $user_info->email);
     }
